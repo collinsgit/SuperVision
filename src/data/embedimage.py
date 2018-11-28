2c316eb5950f93eed9593966237d48bbe62f29de
Julie Ganeshan <ganeshan@mit.edu>
2018-11-27 22:52:55 -0500

    Make a embedder

---
+++ src/data/embedimage.py
@@ -0,0 +1,32 @@
+from model import common
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+class VGG(nn.Module):
+    def __init__(self):
+        rgb_range=1
+        conv_index='54'
+        super(VGG, self).__init__()
+        vgg_features = models.vgg19(pretrained=True).features
+        modules = [m for m in vgg_features]
+        if conv_index.find('22') >= 0:
+            self.vgg = nn.Sequential(*modules[:8])
+        elif conv_index.find('54') >= 0:
+            self.vgg = nn.Sequential(*modules[:35])
+
+        vgg_mean = (0.485, 0.456, 0.406)
+        vgg_std = (0.229 * rgb_range, 0.224 * rgb_range, 0.225 * rgb_range)
+        self.sub_mean = common.MeanShift(rgb_range, vgg_mean, vgg_std)
+        self.vgg.requires_grad = False
+
+    def forward(self, sr, hr):
+        def _forward(x):
+            x = self.sub_mean(x)
+            x = self.vgg(x)
+            return x
+            
+        vgg_sr = _forward(sr)
+        return vgg_sr
---
+++ src/im2vec.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+
+from data import embedimage
+
+
+
+em = embedimage.VGG()
