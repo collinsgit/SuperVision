338e00aed10ddca664c765d40c8975bfc69730a6
Collin Potts <collinpotts@gmail.com>
2018-11-27 21:25:05 -0500

    Stuff that's probably not broken

--- src/data/coco.py
+++ src/data/coco.py
@@ -1,5 +1,7 @@
 import os
+import json
 from data import srdata
+
 
 class Coco(srdata.SRData):
     def __init__(self, args, name='coco',train=True,benchmark=False):
@@ -15,11 +17,24 @@ class Coco(srdata.SRData):
         self.begin, self.end = list(map(lambda x: int(x), data_range))
 
         super().__init__(args, name=name, train=train, benchmark=benchmark)
+
     def _set_filesystem(self, dir_data):
         self.apath = os.path.join(dir_data, 'coco')
         self.dir_hr=os.path.join(self.apath,'hr')
         self.dir_lr=os.path.join(self.apath,'lr')
         self.ext = ('.jpg','.jpg')
+
     def _scan(self):
-        names_hr, names_lr = super()._scan
-        return names_hr[self.begin:self.end], names_lr[self.begin:self.end]
+        names_hr, names_lr = super()._scan()
+        return names_hr[self.begin:self.end], [names_lr[i][self.begin:self.end] for i in range(len(names_lr))]
+
+
+class CocoClasses:
+    def __init__(self, dir_data, cap_dir):
+        path = os.path.join(dir_data, 'coco', cap_dir)
+        annotations = json.load(open(path))['annotations']
+        for entry in annotations:
+
+    def __getitem__(self, filename):
+        pass
+
