b2ee96e69bfdb36e2f5c84b8a2da3dd800927716
Collin Potts <collinpotts@gmail.com>
2018-11-27 21:53:54 -0500

    Added CocoClasses, to get image category

--- src/data/coco.py
+++ src/data/coco.py
@@ -2,6 +2,8 @@ import os
 import json
 from data import srdata
 
+CATEGORIES_FILE = 'annotations/instances_val2017.json'
+
 
 class Coco(srdata.SRData):
     def __init__(self, args, name='coco',train=True,benchmark=False):
@@ -16,6 +18,8 @@ class Coco(srdata.SRData):
 
         self.begin, self.end = list(map(lambda x: int(x), data_range))
 
+        self.classes = CocoClasses(args.dir_data, CATEGORIES_FILE)
+
         super().__init__(args, name=name, train=train, benchmark=benchmark)
 
     def _set_filesystem(self, dir_data):
@@ -29,12 +33,15 @@ class Coco(srdata.SRData):
         return names_hr[self.begin:self.end], [names_lr[i][self.begin:self.end] for i in range(len(names_lr))]
 
 
-class CocoClasses:
+class CocoClasses(object):
     def __init__(self, dir_data, cap_dir):
         path = os.path.join(dir_data, 'coco', cap_dir)
         annotations = json.load(open(path))['annotations']
+
+        self.categories = {}
         for entry in annotations:
+            self.categories[entry['image_id']] = entry['category_id']
 
     def __getitem__(self, filename):
-        pass
+        return self.categories[int(filename.split('.')[0].lstrip('0'))]
 
